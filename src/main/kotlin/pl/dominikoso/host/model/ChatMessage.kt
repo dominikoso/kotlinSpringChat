0fbe559e6a3a5911dd1b0bb8165c8bc1d897e15a
Dominik K <dominikoso@users.noreply.github.com>
2019-01-08 14:06:46 +0100

    Safety update #1

--- src/main/kotlin/pl/dominikoso/host/model/ChatMessage.kt
+++ src/main/kotlin/pl/dominikoso/host/model/ChatMessage.kt
@@ -1,5 +1,11 @@
 package pl.dominikoso.host.model
 
-data class ChatMessage (val type : MessageType, val sender : String?, val content : String?){
+import javax.validation.constraints.Size
+
+data class ChatMessage (val type : MessageType,
+                        @Size(min=2,max=20, message = "Length of username must be greater than or equal 2 and less than or equal 20")
+                        val sender : String?,
+                        @Size(max = 300, message = "Maximum length of text message is 300 characters")
+                        val content : String?){
     enum class MessageType {CHAT, JOIN, LEAVE}
 }
